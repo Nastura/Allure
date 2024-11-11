dbd9d57edb7dff43050f34619d5f0901abcfa1ba
Anastasiya-Vavilova <ntttru@bk.ru>
2024-11-11 07:57:14 +0300

    finalization

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -11,6 +11,7 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,7 +20,7 @@ class DeliveryTest {
 
     @BeforeEach
     void setup() {
-        Configuration.headless = true;
+//        Configuration.headless = true;
         open("http://localhost:9999");
 //        $("[data-test-id='date' input]").sendKeys(Keys.chord(Keys.CONTROL + "A"));
 //        $("[data-test-id='date' input]").sendKeys(Keys.chord("DELETE"));
@@ -42,10 +43,14 @@ class DeliveryTest {
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $(".notification__title").shouldBe(visible, Duration.ofSeconds(5)).shouldBe(exactText("Успешно!"));
+        $(".notification .notification__content").shouldBe(exactText("Встреча успешно запланирована на " + firstMeetingDate));
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(secondMeetingDate);
         $(".button").click();
         $("[data-test-id='replan-notification'] .notification__title").shouldBe(visible, Duration.ofSeconds(5)).shouldBe(exactText("Необходимо подтверждение"));
+        $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible);
         $("[data-test-id='replan-notification'] .button").click();
+        $(withText("Успешно!")).shouldBe(visible);
+        $(".notification .notification__content").shouldBe(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
